76707b9c489a3d9f087808f59cfc0861112396e8
Luis A Mireles <luui9mirel@gmail.com>
2017-11-07 14:04:35 -0700

    Update wdInterruptHandler.c

--- project/project_demo/wdInterruptHandler.c
+++ project/project_demo/wdInterruptHandler.c
@@ -1,26 +1,11 @@
 #include <msp430.h>
-#include "buzzer.h"
-#include "led.h"
-
-void decisecond() 
-{
-  static char cnt = 0;		/* # deciseconds/frequecy change */
-  if (++cnt > 2) {		
-    buzzer_advance_frequency();
-    cnt = 0;
-  }
-}
+#include "stateMachines.h"
 
 void
 __interrupt_vec(WDT_VECTOR) WDT(){	/* 250 interrupts/sec */
-  static char second_count = 0, decisecond_count = 0;
-  if (++second_count == 250) {
-    led_toggle();
-    second_count = 0;
+  static char blink_count = 0;
+  if (++blink_count == 125) {
+    state_advance();
+    blink_count = 0;
   }
-  if (++decisecond_count == 25) {
-    buzzer_advance_frequency();
-    decisecond_count = 0;
-  }
-  led_update();
 }
