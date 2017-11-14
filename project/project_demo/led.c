c2108fbd0d1f901be3f3620d198a05c7daf64f96
luis9am <luui9mirel@gmail.com>
2017-11-14 14:53:58 -0700

    fixed led functions

--- project/project_demo/led.c
+++ project/project_demo/led.c
@@ -17,8 +17,8 @@ void led_update()
   if (switch_state_changed) {
     char ledFlags = 0; /* flags start at 0 */
     
-    ledFlags ^= (switch_state_changed) ? LED_RED : 0;
-    ledFlags ^= (switch_state_changed) ? 0 : LED_GREEN;
+    ledFlags ^= switch_state_down ? LED_RED : 0;
+    ledFlags ^= switch_state_down ? 0 : LED_GREEN;
     
     P1OUT &= (0xff - LEDS) | ledFlags; // clear bit for off leds
     P1OUT ^= ledFlags;		     // set bit for on leds
